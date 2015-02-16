b90b92f01d49c3f7bbae46bc59434a0976f9c164
Paul Laibach <github@laibach.net>
2015-02-16 14:32:27 -0500

    Lab 20150216 full demo query
    Combines multiple sequential operations from DROP table to SELECT

--- lab_00.sql
+++ lab_00.sql
@@ -1,24 +1,50 @@
+﻿DROP TABLE cars;
+
 CREATE TABLE cars (
+   mfgYear int,
    kind text,
    make text,
    model text,
    priceUSD numeric
-   )
+   );
 
-SELECT *
-FROM cars
+-- Demonstrate two different statements to insert data
+INSERT INTO cars (model, kind, mfgYear, make, priceUSD) VALUES
+   ('Wranger', 'Utility', 2013, 'Jeep', 35000),
+   ('SuperDuty', 'Pickup', 2008, 'Ford', 45000);
+INSERT INTO cars (mfgYear, kind, make, model, priceUSD) VALUES
+   (1964, 'Sedan', 'Dodge', 'Polara', 2000),
+   (1963, 'Sedan', 'Volvo', 'PV 544', 1500);
+
+ALTER TABLE cars
+   ADD sellUSD numeric DEFAULT NULL;
+
+-- ALTER RENAME cannot be applied simultaneously?
+ALTER TABLE cars 
+   RENAME priceUSD to costUSD;
+ALTER TABLE cars 
+   RENAME kind to bodyStyle;
+
+-- UPDATE SET cannot be applied simultaneously.
+UPDATE cars
+   SET sellUSD = costUSD * 1.5
+   WHERE mfgYear < 1970;
+UPDATE cars
+   SET sellUSD = costUSD * 1.25
+   WHERE mfgYear >= 1970;
+
+SELECT mfgYear, make, model, bodyStyle, costUSD, sellUSD
+   FROM cars
+
+-- ******************************************************
+-- RANDOM NOTES BELOW HERE
+-- ******************************************************
 
 SELECT make, priceUSD
 FROM cars
 WHERE priceUSD >= 100000
    or model = 'Esprit'
    or kind = 'sedan'
-
-INSERT INTO cars (kind, make, model, priceUSD)
-   VALUES ('sedan', 'Dodge', 'Polara', 2000)
-
-INSERT INTO cars (model, kind, make, costUSD, sellUSD)
-   VALUES ('SuperDuty', 'pickup', 'Fordx', 45000, costUSD * 1.5)
 
 UPDATE cars
 SET sellUSD = priceUSD * 1.5
@@ -32,3 +58,10 @@ ADD sellUSD numeric DEFAULT 9
 
 ALTER TABLE cars
 RENAME priceUSD to costUSD
+
+ALTER TABLE cars
+RENAME costUSD to priceUSD
+
+UPDATE cars
+SET make = 'Ford'
+WHERE make = 'Ford'
