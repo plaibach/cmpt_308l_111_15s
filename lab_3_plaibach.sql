1219418fcbe3a26fcddcb4bfbea7e5af1f9e6aa7
Paul Laibach <github@laibach.net>
2015-03-10 21:47:02 -0400

    lab_3_plaibach
    Become accustomed to writing SQL queries.
    • Enjoy the beauty and accuracy of the relational model.
    • Get some still-easy lab points.
    
    Paul Laibach
    Lab 3
    Getting Started with SQL Queries
    Database Systems
    CMPT 308 - Spring 2015
    Alan G. Labouseur

---
+++ lab_3_plaibach.sql
@@ -0,0 +1,55 @@
+﻿/*Paul Laibach
+  Lab 3
+  Getting Started with SQL Queries
+  Database Systems
+  CMPT 308 - Spring 2015
+  Alan G. Labouseur*/
+
+--1
+SELECT ordno, dollars
+FROM orders;
+
+--2
+SELECT name, city
+FROM agents
+WHERE name = 'Smith';
+
+--3
+SELECT pid, name, priceUSD
+FROM products
+WHERE quantity > 200000;
+
+--4
+SELECT name, city
+FROM customers
+WHERE city = 'Dallas';
+
+--5
+SELECT name
+FROM agents
+WHERE city != 'Tokyo'
+  AND city != 'New York';
+
+--6
+SELECT *
+FROM products
+WHERE city != 'Dallas'
+  AND city != 'Duluth'
+  AND priceUSD >= 1;
+
+--7
+SELECT *
+FROM orders
+WHERE mon = 'jan'
+   OR mon = 'may'
+
+--8
+SELECT *
+FROM orders
+WHERE mon = 'feb'
+  AND dollars > 500
+
+--9
+SELECT ordno
+FROM orders
+WHERE cid = 'c005'
