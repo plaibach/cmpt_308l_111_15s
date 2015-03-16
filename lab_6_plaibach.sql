56f366f990140910e28eeac0981ec11b984a505c
Paul Laibach <github@laibach.net>
2015-03-16 14:41:56 -0400

    lab_6_plaibach
    • Write some downright difficult SQL queries
    • Work really hard for lab points.
    
    Paul Laibach
    Lab 6
    Interesting and Painful Queries
    Database Systems
    CMPT 308 - Spring 2015
    Alan G. Labouseur

---
+++ lab_6_plaibach.sql
@@ -0,0 +1,34 @@
+﻿/* Paul Laibach
+   Lab 6
+   Interesting and Painful Queries
+   Database Systems
+   CMPT 308 - Spring 2015
+   Alan G. Labouseur */
+
+-- 1. Display the name and city of customers who live in any city that makes the most different kinds of products. (There are two cities that make the most different products. Return the name and city of customers from either one of those.) 
+
+   
+
+-- 2. Display the names of products whose priceUSD is below the average priceUSD, in alphabetical order. 
+
+   
+
+-- 3. Display the customer name, pid ordered, and the dollars for all orders, sorted by dollars from high to low. 
+
+   
+
+-- 4. Display all customer names (in reverse alphabetical order) and their total ordered, and nothing more. Use coalesce to avoid showing NULLs. 
+
+   
+
+-- 5. Display the names of all customers who bought products from agents based in Tokyo along with the names of the products they ordered, and the names of the agents who sold it to them. 
+
+   
+
+-- 6. Write a query to check the accuracy of the dollars column in the Orders table. This means calculating Orders.dollars from data in other tables and comparing those values to the values in Orders.dollars. Display all rows in Orders where Orders.dollars is incorrect, if any. 
+
+   
+
+-- 7. What’s the difference between a LEFT OUTER JOIN and a RIGHT OUTER JOIN? Give example queries in SQL to demonstrate. (Feel free to use the CAP2 database t make your points here.) 
+
+   
